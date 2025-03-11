33e352e9ad1733239be1a22a9b31cfaf3e100b49
TheDeepDaemon <thedeepdaemon@gmail.com>
2025-03-10 20:03:54 -0400

    commented the archiver

--- vis_archiver.py
+++ vis_archiver.py
@@ -3,8 +3,10 @@ import shutil
 
 if __name__=="__main__":
 
+    # create an archive directory if it doesn't exist
     os.makedirs("vis_archive", exist_ok=True)
 
+    # move each file in the "vis_output" directory over to the archive
     for fname in os.listdir("vis_output"):
         source = os.path.join("vis_output", fname)
         destination = os.path.join("vis_archive", fname)

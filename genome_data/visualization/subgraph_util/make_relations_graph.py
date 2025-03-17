bcb268bfeabeab03bc299a755b8e3ec78178ec73
TheDeepDaemon <thedeepdaemon@gmail.com>
2025-03-17 13:35:10 -0400

    changes to subgraph logic

--- genome_data/visualization/subgraph_util/make_relations_graph.py
+++ genome_data/visualization/subgraph_util/make_relations_graph.py
@@ -1,6 +1,15 @@
 import networkx as nx
 
 def make_relations_graph(genome_data):
+    """
+    Build the graph that represents all relations between genomes.
+
+    Args:
+        genome_data: The dictionary of all genome data.
+
+    Returns:
+        The relations graph.
+    """
     graph = nx.DiGraph()
 
     for gid in genome_data:
@@ -8,6 +17,7 @@ def make_relations_graph(genome_data):
 
     for gid, data in genome_data.items():
         for parent in data['parents']:
-            graph.add_edge(int(parent), gid)
+            if parent in genome_data:
+                graph.add_edge(int(parent), gid)
 
     return graph
